217f33188b7f8ccac891c20db6a19db20137866b
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2020-04-03 16:09:11 +0530

    7th commit

--- lib/main.dart
+++ lib/main.dart
@@ -126,6 +126,58 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                   ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        height: 65.0,
+                        width: 60.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                              width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(
+                          child: Icon(Icons.search, color: Colors.black),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 60.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                              width: 1.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(
+                          child:
+                              Icon(Icons.shopping_basket, color: Colors.black),
+                        ),
+                      ),
+                      Container(
+                        height: 65.0,
+                        width: 120.0,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.grey,
+                                style: BorderStyle.solid,
+                                width: 1.0),
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Color(0xFF1C1428)),
+                        child: Center(
+                            child: Text('Checkout',
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    color: Colors.white,
+                                    fontSize: 15.0))),
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
@@ -168,7 +220,7 @@ Widget _buildFoodItem(String imgpath, String foodName, String price) {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      foodName,
+                      price,
                       style: TextStyle(
                         fontFamily: 'Montserrat',
                         fontSize: 15.0,
