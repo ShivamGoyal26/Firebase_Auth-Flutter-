77b01140ecc23026b5d523d900fa28a5b26e164f
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2020-04-07 00:36:11 +0530

    12th commit

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -94,7 +94,7 @@ class _DetailsPageState extends State<DetailsPage> {
                         ),
                       ),
                       SizedBox(
-                        height: 20,
+                        height: 30,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -111,6 +111,62 @@ class _DetailsPageState extends State<DetailsPage> {
                             height: 25,
                             color: Colors.grey,
                             width: 1.0,
+                          ),
+                          Container(
+                            width: 125.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17.0),
+                                color: Color(0xFF7A9BEE)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(7),
+                                      color: Color(0xFF7A9BEE),
+                                    ),
+                                    child: Center(
+                                      child: Icon(
+                                        Icons.remove,
+                                        color: Colors.white,
+                                        size: 20.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  '2',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () {},
+                                  child: Container(
+                                    height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(7.0),
+                                      color: Colors.white,
+                                    ),
+                                    child: Center(
+                                      child: Icon(
+                                        Icons.add,
+                                        color: Color(0xFF7A9BEE),
+                                        size: 20.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       )
